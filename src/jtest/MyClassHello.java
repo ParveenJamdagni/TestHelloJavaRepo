54d131d701f313fe5ccaf6fdfd475396bc8e6cdb
Praveen .3.Kumar <PK072166ipc@TechmIPC.com>
2017-08-21 13:00:29 +0530

    added hello world java project

---
+++ src/jtest/MyClassHello.java
@@ -0,0 +1,10 @@
+package jtest;
+
+public class MyClassHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is a test Jenkins program");
+	}
+
+}
