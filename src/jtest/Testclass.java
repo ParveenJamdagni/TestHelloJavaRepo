14093d4aa0233f302dc7f3ef4a7c975863789c41
Praveen .3.Kumar <PK072166ipc@TechmIPC.com>
2017-08-23 16:34:16 +0530

    New java file added

--- src/jtest/Testclass.java
+++ src/jtest/Testclass.java
@@ -1,10 +1,10 @@
 package jtest;
 
-public class MyClassHello {
+public class Testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is a test Jenkins progragggm");
+
 	}
 
 }
