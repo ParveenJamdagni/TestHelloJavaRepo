5ff2e2cb0f6a2a49eda3733284b7b576aad38373
PK072166ipc <PK072166ipc@INNOSZPC00712.TechmIPC.com>
2017-08-25 17:34:42 +0530

    New comment added in java file

--- src/jtest/MyClassHello.java
+++ src/jtest/MyClassHello.java
@@ -5,7 +5,7 @@ public class MyClassHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is a test Jenkins progragggm");
-System.out.println("Welocme to jenkins******************************************");
+System.out.println("Welocme to jenkins***&&&&&&&&&&&&&&&&&&&&&*********************");
 	}
 
 }
